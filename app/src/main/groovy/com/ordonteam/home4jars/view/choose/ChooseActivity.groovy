7b48074190dcce8f57e523f764b3d7cb64e8cac0
Ordon <ordon1313@gmail.com>
2015-05-16 11:32:07 +0200

    Start choosing activity

--- app/src/main/groovy/com/ordonteam/home4jars/view/choose/ChooseActivity.groovy
+++ app/src/main/groovy/com/ordonteam/home4jars/view/choose/ChooseActivity.groovy
@@ -1,9 +1,15 @@
 package com.ordonteam.home4jars.view.choose
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import groovy.transform.CompileStatic
 
 @CompileStatic
 final class ChooseActivity extends Activity {
 
+    static void start(Context context) {
+        Intent intent = new Intent(context, ChooseActivity)
+        context.startActivity(intent)
+    }
 }
