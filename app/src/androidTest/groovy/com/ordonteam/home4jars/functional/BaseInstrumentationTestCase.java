ffa0e226f0102702a177652e17fadc97f51a5bc3
Ordon <ordon1313@gmail.com>
2015-05-17 15:32:26 +0200

    Testy

--- app/src/androidTest/groovy/com/ordonteam/home4jars/functional/BaseInstrumentationTestCase.java
+++ app/src/androidTest/groovy/com/ordonteam/home4jars/functional/BaseInstrumentationTestCase.java
@@ -2,13 +2,13 @@ package com.ordonteam.home4jars.functional;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.ordonteam.home4jars.view.depercated.search.SearchActivity;
+import com.ordonteam.home4jars.MainActivity;
 
 
-public abstract class BaseInstrumentationTestCase extends ActivityInstrumentationTestCase2<SearchActivity> {
+public abstract class BaseInstrumentationTestCase extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public BaseInstrumentationTestCase() {
-        super(SearchActivity.class);
+        super(MainActivity.class);
     }
 
 }
