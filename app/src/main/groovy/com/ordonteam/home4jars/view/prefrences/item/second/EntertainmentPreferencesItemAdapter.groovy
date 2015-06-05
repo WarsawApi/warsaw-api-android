cbf52f068d67aafdf5c4c4c39f8a50bcb14197a9
Ordon <ordon1313@gmail.com>
2015-06-05 13:49:13 +0200

    EntertainmentPreferencesItemAdapter

--- app/src/main/groovy/com/ordonteam/home4jars/view/prefrences/item/second/EntertainmentPreferencesItemAdapter.groovy
+++ app/src/main/groovy/com/ordonteam/home4jars/view/prefrences/item/second/EntertainmentPreferencesItemAdapter.groovy
@@ -3,11 +3,14 @@ package com.ordonteam.home4jars.view.prefrences.item.second
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.ordonteam.home4jars.R
+import com.ordonteam.home4jars.dto.preferences.EntertainmentPreferences
 import com.ordonteam.home4jars.view.common.ItemAdapter
 import groovy.transform.CompileStatic
 
 @CompileStatic
 final class EntertainmentPreferencesItemAdapter extends ItemAdapter<Holder> {
+
+    EntertainmentPreferences entertainmentPreferences = new EntertainmentPreferences()
 
     @Override
     int getViewType() {
@@ -21,13 +24,40 @@ final class EntertainmentPreferencesItemAdapter extends ItemAdapter<Holder> {
 
     @Override
     void onBindViewHolder(Holder holder) {
+        holder.clubs.selected = entertainmentPreferences.clubs
+        holder.clubs.onClickListener = this.&onClubsClick
+        holder.pubs.selected = entertainmentPreferences.pubs
+        holder.pubs.onClickListener = this.&onPubsClick
+        holder.restaurants.selected = entertainmentPreferences.restaurants
+        holder.restaurants.onClickListener = this.&onRestaurantsClick
+    }
 
+    void onClubsClick(View view){
+        view.selected = !view.selected
+        entertainmentPreferences.clubs = view.selected
+    }
+
+    void onPubsClick(View view){
+        view.selected = !view.selected
+        entertainmentPreferences.pubs = view.selected
+    }
+
+    void onRestaurantsClick(View view){
+        view.selected = !view.selected
+        entertainmentPreferences.restaurants = view.selected
     }
 
     final static class Holder extends RecyclerView.ViewHolder {
 
+        View clubs
+        View pubs
+        View restaurants
+
         Holder(View itemView) {
             super(itemView)
+            clubs = itemView.findViewById(R.id.entertainment_clubs)
+            pubs = itemView.findViewById(R.id.entertainment_pubs)
+            restaurants = itemView.findViewById(R.id.entertainment_restaurants)
         }
     }
 }
